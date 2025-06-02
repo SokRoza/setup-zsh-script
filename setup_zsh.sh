8909d3807206aad6ec4a160f4009be244c57b932
SokRoza <yengsokroza2023@gmail.com>
2025-06-02 10:07:18 +0700

    setup script

---
+++ setup_zsh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install zsh -y
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' .zshrc
+
+source .zshrc 
